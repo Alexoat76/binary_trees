edb3ccbf056f4c27f75fba08a7ce2f8da00fa8bc
Alexoat76 <3915@holbertonschool.com>
2022-03-23 09:48:24 -0500

    :white_check_mark:Creates a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/*
+ * File: 0-binary_tree_node.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+/**
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: The parent node of the new binary tree node.
+ * @value: The value of the new binary tree node.
+ * Return: A pointer to the created node, otherwise NULL.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node != NULL)
+	{
+		new_node->left = NULL;
+		new_node->right = NULL;
+		new_node->parent = parent;
+		new_node->n = value;
+	}
+	return (new_node);
+}
