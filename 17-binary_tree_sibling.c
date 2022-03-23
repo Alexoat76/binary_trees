6cb7570a2e3fae25b908d5999d6eac2eece60aab
Alexoat76 <3915@holbertonschool.com>
2022-03-23 17:22:26 -0500

    Finds the sibling of a node :heavy_check_mark:

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/*
+ * File: 17-binary_tree_sibling.c
+ * Author: Alex O. Arevalo T.
+ * email: 3915@holbertonschool.com
+ */
+
+/**
+ * binary_tree_sibling - Finds the sibling of a binary tree's node.
+ * @node: The node in the binary tree.
+ * Return: The sibling of the given node.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *left = NULL;
+	binary_tree_t *right = NULL;
+
+	if ((node != NULL) && (node->parent != NULL))
+	{
+		left = node->parent->left;
+		right = node->parent->right;
+	}
+	return (left == node ? right : left);
+}
